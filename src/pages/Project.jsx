c46864d5acd7c3da9e742344a4268b61a76c8486
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-04-05 17:19:43 +0530

    project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -4,13 +4,13 @@ import diaphoto from "../assets/diabetes.jpg";
 import roadphoto from "../assets/roadsafety.jpeg";
 import sales from "../assets/sales.jpeg";
 import reactphoto from "../assets/reactport.jpg";
-import htmlphoto  from "../assets/Htmlport.jpg";
-import todophoto  from "../assets/to-do.jpg";
-
+import htmlphoto from "../assets/Htmlport.jpg";
+import todophoto from "../assets/to-do.jpg";
+import { Link } from "react-router-dom";
 
 const Project = () => {
   return (
-    <div className="bg-black h-screen w-full flex flex-col items-center gap-10  text-white text-4xl pt-24">
+    <div className="bg-black h-screen w-full flex flex-col items-center gap-10  text-white text-4xl pt-24 pl-20 pr-20">
       <p
         data-aos="fade-up"
         data-aos-delay="100"
@@ -23,21 +23,23 @@ const Project = () => {
         data-aos="fade-up"
         data-aos-delay="200"
       >
-        <Card
-          photo={diaphoto}
-          heading="Diabetes Prediction"
-          text="Pandas Numpy Matplotlib Scikit-learn Seaborn"
-        />
-        <Card
-          photo={roadphoto}
-          heading="Road Accident Data Analysis"
-          text="MS Excel"
-        />
-        <Card
-          photo={sales}
-          heading="Sales Analysis"
-          text="PowerBI"
-        />
+        <Link to="https://github.com/Himanshipatel04/Notebook/blob/main/Diabetes_project.ipynb">
+          <Card
+            photo={diaphoto}
+            heading="Diabetes Prediction"
+            text="Pandas Numpy Matplotlib Scikit-learn Seaborn"
+          />
+        </Link>
+        <Link to="https://github.com/Himanshipatel04/Road_safety_analysis">
+          <Card
+            photo={roadphoto}
+            heading="Road Accident Data Analysis"
+            text="MS Excel"
+          />
+        </Link>
+        <Link to="#">
+          <Card photo={sales} heading="Sales Analysis" text="PowerBI" />
+        </Link>
       </div>
 
       <div
@@ -45,21 +47,17 @@ const Project = () => {
         data-aos="fade-up"
         data-aos-delay="300"
       >
-        <Card
-          photo={reactphoto}
-          heading="Portfolio Website"
-          text="React"
-        />
-        <Card
+        <Link to="https://portfolioinreact.vercel.app/">
+          <Card photo={reactphoto} heading="Portfolio Website" text="React" />
+        </Link>
+        <Link to="https://github.com/Himanshipatel04/To-do-List"><Card
           photo={todophoto}
           heading="To-do list"
           text="HTML,CSS,JavaScript"
-        />
-        <Card
-          photo={htmlphoto}
-          heading="Portfolio Website"
-          text="HTML,CSS"
-        />
+        /></Link>
+        <Link to="https://himanshipatel04.github.io/Portfolio/">
+          <Card photo={htmlphoto} heading="Portfolio Website" text="HTML,CSS" />
+        </Link>
       </div>
     </div>
   );
